31388565b8bce25b0a58d031ebe440c5ebdb0c18
Sidharthan624 <111054733+Sidharthan624@users.noreply.github.com>
2024-01-10 12:45:12 +0530

    dbconnect

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,8 +3,7 @@ const mongoose=require('mongoose')
 const dbConnection=()=>{
     try {
         const connect=mongoose.connect(`${process.env.MONGO_DB}`,{
-            // useNewUrlParser:true,
-            // useUnifiedTopology:true
+            
         })
           console.log("Connected to mongodb")
     } catch (error) {
