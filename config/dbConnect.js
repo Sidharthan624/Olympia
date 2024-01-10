4d01f0dea33f947ad0c61d79706f7ba7d55ca7aa
Sidharthan624 <111054733+Sidharthan624@users.noreply.github.com>
2024-01-10 13:14:46 +0530

    db connection string changed

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,7 @@ const mongoose=require('mongoose')
 
 const dbConnection=()=>{
     try {
-        const connect=mongoose.connect(`${process.env.MONGO_DB}`,{
-            // useNewUrlParser:true,
-            // useUnifiedTopology:true
-        })
+        const connect=mongoose.connect(`${process.env.MONGO_DB}`)
           console.log("Connected to mongodb")
     } catch (error) {
         console.log("Mongodb connection error:",err);
